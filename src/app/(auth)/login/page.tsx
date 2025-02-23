2c57f2498e68cb81a6933cba34acee5d56976169
simranpatthak <simran.pathak0707@gmail.com>
2025-02-23 20:00:04 +0530

    initial push

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,17 +1,22 @@
-"use client"
-import AuthPage from '@/components/base/AuthPage'
-import { useSearchParams } from 'next/navigation'
-import React from 'react'
-import { Suspense } from "react";
+"use client";
+import AuthPage from "@/components/base/AuthPage";
+import { useSearchParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 const Login = () => {
-    const searchParams = useSearchParams()
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-        {searchParams.get('message')? <h4 className='bg-green-300 font-bold rounded-md p-4'>{searchParams.get('message')}</h4>:""}
-        <AuthPage type={"login"}/>
-        </Suspense>
-  )
-}
+  const searchParams = useSearchParams();
+  const [message, setMessage] = useState<string | null>(null);
 
-export default Login
+  useEffect(() => {
+    setMessage(searchParams.get("message"));
+  }, [searchParams]);
+
+  return (
+    <div>
+      {message ? <h4 className="bg-green-300 font-bold rounded-md p-4">{message}</h4> : ""}
+      <AuthPage type={"login"} />
+    </div>
+  );
+};
+
+export default Login;
