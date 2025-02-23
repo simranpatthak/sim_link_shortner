4f2271dc5adc6e6232d6b1eacf65e303c51c0034
simranpatthak <simran.pathak0707@gmail.com>
2025-02-23 20:10:56 +0530

    initial push

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ Build time ESLint check disable
+  },
+  typescript: {
+    ignoreBuildErrors: true, // ✅ TypeScript errors ko ignore karega
+  }, reactStrictMode: false,
 };
 
 export default nextConfig;
